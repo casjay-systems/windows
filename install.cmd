0f3476fc7db3e04a0e217eb418ea2bf61aee3bcb
casjay <git-admin@casjaysdev.com>
2020-08-07 00:52:48 -0400

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,7 +1,6 @@
 @echo off
 color 1F
 echo.
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 powershell.exe -Command Invoke-WebRequest https://github.com/casjay-systems/windows/raw/master/src/os/just-install.exe -o %USERPROFILE%\Downloads\just-install.exe
 
 %USERPROFILE%\Downloads\just-install.exe /qn
@@ -10,6 +9,7 @@ git clone -q https://github.com/powerline/fonts %USERPROFILE%\Downloads\fonts
 git clone -q https://github.com/casjay-systems/windows %USERPROFILE%\Downloads\windows
 
 powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\scoop.ps1"
+powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\chocolatey.ps1"
 powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\fonts\install.ps1"
 
 Install-Module posh-git -Scope CurrentUser
@@ -20,7 +20,5 @@ Import-Module posh-git
 Import-Module oh-my-posh
 
 Set-Theme Powerlevel10k-Classic
-
-choco install -y chocolateygui nvm nodejs
 
 nvm install node@13.12.0
