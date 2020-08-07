f8f721391a8330a2731c99404389f0f1a5f9fdca
casjay <git-admin@casjaysdev.com>
2020-08-07 13:11:48 -0400

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -3,10 +3,10 @@ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 Start-Process git "clone -q https://github.com/powerline/fonts $env:userprofile\Downloads\fonts" -NoNewWindow -Wait
 Start-Process git "clone -q https://github.com/casjay-systems/windows $env:userprofile\Downloads\windows" -NoNewWindow -Wait
 
-Copy-Item -Path "$env:userprofile\Downloads\windows\src\git\*" -Destination "$env:userprofile" -Recurse
-Copy-Item -Path "$env:userprofile\Downloads\windows\src\bash\*" -Destination "$env:userprofile" -Recurse
-Copy-Item -Path "$env:userprofile\Downloads\windows\src\powershell\*" -Destination "$env:userprofile\Documents\PowerShell" -Recurse
-Copy-Item -Path "$env:userprofile\Downloads\windows\src\powershell\*" -Destination "$env:userprofile\Documents\WindowsPowerShell" -Recurse
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\git\*" -Destination "$env:userprofile" -Recurse -Force
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\bash\*" -Destination "$env:userprofile" -Recurse -Force
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\powershell\*" -Destination "$env:userprofile\Documents\PowerShell" -Recurse -Force
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\powershell\*" -Destination "$env:userprofile\Documents\WindowsPowerShell" -Recurse -Force
 
 Invoke-Expression $env:userprofile\Downloads\fonts\install.ps1
 Invoke-Expression $env:userprofile\Downloads\windows\src\os\scoop.ps1
