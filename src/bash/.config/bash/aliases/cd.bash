e7c0a53a0eb927b9610a2741820094ec25749483
casjay <git-admin@casjaysdev.com>
2020-08-07 12:36:03 -0400

    Create cd.bash

---
+++ src/bash/.config/bash/aliases/cd.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# CD Aliases
+alias ~="cd $HOME"                              #cd home
+alias ..="cd .."                                #cd fix
+alias ...="cd ../.."                            #cd fix
+alias ....="cd ../../.."                        #cd fix
+alias cd..="cd .."                              #cd fix
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
