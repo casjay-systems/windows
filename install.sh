63739274ef224b334d0044a8299cf2da7ff35351
casjay <git-admin@casjaysdev.com>
2020-08-07 03:07:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,8 @@ case $OS in
     ;;
   'WindowsNT')
     OS='Windows'
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"    ;;
+    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/casjay-systems/windows/master/install.cmd'))"    
+    ;;
   'FreeBSD')
     OS='FreeBSD'
     echo "Not Supported"
