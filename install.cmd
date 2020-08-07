cd69e59ee57b9b03468082f013445e1f5b424414
casjay <git-admin@casjaysdev.com>
2020-08-07 13:10:28 -0400

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -3,15 +3,15 @@ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 Start-Process git "clone -q https://github.com/powerline/fonts $env:userprofile\Downloads\fonts" -NoNewWindow -Wait
 Start-Process git "clone -q https://github.com/casjay-systems/windows $env:userprofile\Downloads\windows" -NoNewWindow -Wait
 
-Copy-Item -Path "src\bash\*" -Destination "$env:userprofile" -Recurse
-Copy-Item -Path "src\git\*" -Destination "$env:userprofile" -Recurse
-Copy-Item -Path "src\powershell\*" -Destination "$env:userprofile\Documents\PowerShell" -Recurse
-Copy-Item -Path "src\powershell\*" -Destination "$env:userprofile\Documents\WindowsPowerShell" -Recurse
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\git\*" -Destination "$env:userprofile" -Recurse
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\bash\*" -Destination "$env:userprofile" -Recurse
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\powershell\*" -Destination "$env:userprofile\Documents\PowerShell" -Recurse
+Copy-Item -Path "$env:userprofile\Downloads\windows\src\powershell\*" -Destination "$env:userprofile\Documents\WindowsPowerShell" -Recurse
 
-Invoke-Expression $env:userprofile\Downloads\windows\src\os\just-install.ps1
-Invoke-Expression $env:userprofile\Downloads\windows\src\os\scoop.ps1
-Invoke-Expression $env:userprofile\Downloads\windows\src\os\chocolatey.ps1
 Invoke-Expression $env:userprofile\Downloads\fonts\install.ps1
+Invoke-Expression $env:userprofile\Downloads\windows\src\os\scoop.ps1
+Invoke-Expression $env:userprofile\Downloads\windows\src\os\just-install.ps1
+Invoke-Expression $env:userprofile\Downloads\windows\src\os\chocolatey.ps1
 
 Install-Module -Name PSReadLine -Force -SkipPublisherCheck
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
