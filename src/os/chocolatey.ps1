6f58f2b9c9904bf48b7855e8bae5c30dfcefdd8f
casjay <git-admin@casjaysdev.com>
2020-08-07 02:26:05 -0400

    Update chocolatey.ps1

--- src/os/chocolatey.ps1
+++ src/os/chocolatey.ps1
@@ -2,4 +2,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y chocolateygui cascadiacodepl firacodepl --params "ALLUSERS=1"
-choco install -y 7zip curl sudo git openssh coreutils grep sed less python ruby go nodejs --params "ALLUSERS=1"
+choco install -y 7zip curl sudo git openssh coreutils grep sed less perl php python ruby go nodejs --params "ALLUSERS=1"
