92e86e3591f526cc62341c21cede2541d2d23105
casjay <git-admin@casjaysdev.com>
2020-08-07 03:02:28 -0400

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -4,18 +4,18 @@ echo.
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
 
 git clone -q https://github.com/powerline/fonts %USERPROFILE%\Downloads\fonts
-git clone -q https://github.com/casjay-systems/windows %USERPROFILE%\Downloads\windows /n
+git clone -q https://github.com/casjay-systems/windows %USERPROFILE%\Downloads\windows
 
-powershell.exe %USERPROFILE%\Downloads\windows\src\os\just-install.exe
+Start-Process %USERPROFILE%\Downloads\windows\src\os\just-install.exe -NoNewWindow -Wait
 
-powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\scoop.ps1"
-powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\chocolatey.ps1"
-powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\fonts\install.ps1"
+ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\scoop.ps1"
+ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\chocolatey.ps1"
+ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\fonts\install.ps1"
 
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name PowerShellForGitHub
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 
 Import-Module posh-git
 Import-Module oh-my-posh
