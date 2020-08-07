3f0ff811df60291a58a628253c7cd16bd9d635fb
casjay <git-admin@casjaysdev.com>
2020-08-07 13:00:29 -0400

    Update chocolatey.ps1

--- src/os/chocolatey.ps1
+++ src/os/chocolatey.ps1
@@ -2,4 +2,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y chocolateygui cascadiacodepl cascadia-code-nerd-font --params "ALLUSERS=1"
-choco install -y 7zip curl sudo openssh grep sed less --params "ALLUSERS=1"
+choco install -y 7zip curl sudo openssh grep sed less nvm --params "ALLUSERS=1"
