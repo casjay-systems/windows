53bfa7bac8fd9f99dab15b60cbb89b61f6c4acf7
casjay <git-admin@casjaysdev.com>
2020-08-07 02:17:30 -0400

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -2,12 +2,11 @@
 color 1F
 echo.
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
-powershell.exe %USERPROFILE%\Downloads\just-install.exe
-
-%USERPROFILE%\Downloads\just-install.exe /qn
 
 git clone -q https://github.com/powerline/fonts %USERPROFILE%\Downloads\fonts
-git clone -q https://github.com/casjay-systems/windows %USERPROFILE%\Downloads\windows
+git clone -q https://github.com/casjay-systems/windows %USERPROFILE%\Downloads\windows /n
+
+powershell.exe %USERPROFILE%\Downloads\windows\src\os\just-install.exe
 
 powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\scoop.ps1"
 powershell.exe -ExecutionPolicy Bypass -File "%USERPROFILE%\Downloads\windows\src\os\chocolatey.ps1"
