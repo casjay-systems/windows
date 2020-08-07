57d0786f970b301fa650117325c9f4c8283d86d6
casjay <git-admin@casjaysdev.com>
2020-08-07 01:46:18 -0400

    Update Microsoft.PowerShell_profile.ps1

--- src/powershell/Microsoft.PowerShell_profile.ps1
+++ src/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
 Clear-Host
+Set-Prompt
 
 Import-Module posh-git
-
 Import-Module oh-my-posh
 
-Set-Theme Powerlevel10k-Classic
+Set-Theme Paradox
 
 "You are now entering PowerShell : " + $env:Username
