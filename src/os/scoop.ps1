49c7a656cf368faa82427b434549d486b09bd44f
casjay <git-admin@casjaysdev.com>
2020-08-07 02:21:29 -0400

    Update scoop.ps1

--- src/os/scoop.ps1
+++ src/os/scoop.ps1
@@ -4,5 +4,5 @@ iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 scoop install sudo
 scoop bucket add nerd-fonts
-scoop install 7zip curl sudo git openssh coreutils grep sed less python ruby go nodejs --global
+#scoop install 7zip curl sudo git openssh coreutils grep sed less python ruby go nodejs --global
 
